79aef453d6aeb73e53a22a9a0284dab315d551b4
icd_yokoo <yokoo@icd.co.jp>
2025-11-14 16:09:17 +0900

    fix pagination with claude

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,20 @@
 import BlogList from "@/components/blog-list";
 import CategoryFilter from "@/components/category-filter";
 import Pagination from "@/components/pagination";
-import { getBlogs } from "@/lib/client";
-import { getEvents } from "@/lib/client";
+import { getBlogs, getEvents } from "@/lib/client";
 import { LIMIT } from "@/lib/constants";
 import EventList from "@/components/event-list";
 
-export default async function Home() {
-  const { contents: blogs } = await getBlogs({
-    limit: LIMIT,
-    offset: 0,
-  });
-  
-  const { contents: events } = await getEvents({
-    limit: LIMIT,
-    offset: 0,
-  });
+export default async function Page() {
+  const { contents: blogs, totalCount } = await getBlogs();
+  const { contents: events } = await getEvents();
 
   return (
     <div>
       <CategoryFilter />
       <BlogList blogs={blogs} />
       <EventList events={events} />
-      <Pagination />
+      <Pagination totalCount={totalCount} />
     </div>
   );
 }
