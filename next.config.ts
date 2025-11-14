c4cc6001e15cecb3d3ecfd21d66ca03dda49c2d1
icd_yokoo <yokoo@icd.co.jp>
2025-11-14 16:20:40 +0900

    add nex.config.ts file

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    MICROCMS_SERVICE_DOMAIN: process.env.MICROCMS_SERVICE_DOMAIN,
+    MICROCMS_API_KEY: process.env.MICROCMS_API_KEY,
+  },
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "images.microcms-assets.io",
+        protocol: 'https',
+        hostname: 'images.microcms-assets.io',
       },
     ],
   },
